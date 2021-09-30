77603466c005bfcdcc1c1261737b661d687f81f8
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2021-09-30 16:43:11 +0200

    create parallel main

--- src/parallel-main.cpp
+++ src/parallel-main.cpp
@@ -1,53 +1,24 @@
 #include "main.h"
+#include <tuple>
+#include <chrono>
+#include <thread>
+
+// insert function for vector of key-value pairs
+void insert(vector<tuple<float, string>>* inserts, BPlusTree* tree) {
+	for(auto insert : *inserts) {
+		tree->Insert(get<0>(insert), get<1>(insert));
+	}
+}
 
 // main function
 int main(int argc, char* argv[])
 {
-	int m = ERROR, pos = ERROR;
+	int pos = ERROR;
 	float key = ERROR, key1 = ERROR, key2 = ERROR;
 	string line, value;
-	class BPlusTree tree;
+	vector<tuple<float, string>> inserts;
 
-	// check command-line arguments
-	if(NUM_ARGS != argc)
-	{
-		cout<<"Invalid argument!"<<endl;
-		cout<<"Usage: ./treesearch <input_filename>"<<endl;
-		exit(-1);
-	}
-
-	// check if input file is valid
-	ifstream file(argv[1]);
-	if(!file)
-	{
-		cout<<"Invalid input file!"<<endl;
-		exit(-1);
-	}
-
-	// get the B+ tree order
-	getline(file, line);
-	try
-	{
-		m = stoi(line);
-	}
-	catch(...)
-	{
-		cout<<"Invalid B+ tree order!"<<endl;
-		exit(-1);
-	}
-
-	// check if order is atleast 3
-	if(m < MIN_ORDER)
-	{
-		cout<<"Invalid B+ tree order!"<<endl;
-		exit(-1);
-	}
-
-	// initialize the B+ tree of order m
-	tree.Initialize(m);
-
-	// open the output file for writing results
-	tree.Open_Output_File();
+	ifstream file("input_file_single.txt");
 
 	// read the input file and perform the corresponding operations
 	while (getline(file, line))
@@ -57,7 +28,6 @@ int main(int argc, char* argv[])
 		value = ERROR;
 		key1  = ERROR;
 		key2  = ERROR;
-
 		// operation: Insert(key, value)
 		if(0 == line.compare(0, 6, "Insert"))
 		{
@@ -65,43 +35,47 @@ int main(int argc, char* argv[])
 			key = atof(line.substr(7, pos - 7).c_str());
 			value = line.substr(pos + 1, line.size() - pos - 2);
 
-			// insert the key-value pair
-			tree.Insert(key, value);
-		}
-
-		// if it is a search operation
-		else if (0 == line.compare(0, 6, "Search"))
-		{
-			// operation: Search(key)
-			pos = line.find(",");
-			if(ERROR == pos)
-			{
-				key = atof(line.substr(7, line.size() - 8).c_str());
-
-				// search for the key and display the correpsonding value(s)
-				tree.Search(key);
-			}
-
-			// operation: Search(key1, key2)
-			else
-			{
-				key1 = atof(line.substr(7, pos - 7).c_str());
-				key2 = atof(line.substr(pos + 1, line.size() - pos -2).c_str());
-
-				// search for keys between key1 and key2, and display the corresponding value(s)
-				tree.Search(key1, key2);
-			}
+			// add (key, value) to in-memory vector
+			inserts.push_back(make_tuple(key, value));
 		}
 	}
 
-	// close the output file
-	tree.Close_Output_File();
+	// Split inserts into 4 vectors
+	size_t const quarter_size = inserts.size() / 4;
+	vector<tuple<float, string>> inserts1(inserts.begin(), inserts.begin() + quarter_size);
+	vector<tuple<float, string>> inserts2(inserts.begin() + quarter_size, inserts.begin() + 2*quarter_size);
+	vector<tuple<float, string>> inserts3(inserts.begin() + 2*quarter_size, inserts.begin() + 3*quarter_size);
+	vector<tuple<float, string>> inserts4(inserts.begin() + 3*quarter_size, inserts.end());
+
+	// Create and initialize trees
+	BPlusTree tree1;
+	BPlusTree tree2;
+	BPlusTree tree3;
+	BPlusTree tree4;
+	tree1.Initialize(3);
+	tree2.Initialize(3);
+	tree3.Initialize(3);
+	tree4.Initialize(3);
+
+	// Build thread-trees
+	auto t1 = chrono::high_resolution_clock::now();
+	thread thread1 (insert, &inserts1, &tree1);
+	thread thread2 (insert, &inserts2, &tree2);
+	thread thread3 (insert, &inserts3, &tree3);
+	thread thread4 (insert, &inserts4, &tree4);
+	thread1.join();
+	thread2.join();
+	thread3.join();
+	thread4.join();
+	auto t2 = chrono::high_resolution_clock::now();
+	chrono::duration<double, std::milli> ms_double = t2 - t1;
 
 #ifdef DEBUG
 	// print the current state of the B+ tree
 	tree.Print_Tree();
 #endif
 
-	cout<<endl<<"Check "<<OUTPUT_FILE<<" for results!"<<endl<<endl;
+	cout << ms_double.count() << "ms\n";
 	return 0;
 }
+
