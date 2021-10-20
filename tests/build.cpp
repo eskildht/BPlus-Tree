56e824268e157d4a54a0c2c9429e5a9703f93242
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2021-10-20 20:33:11 +0200

    added args to test-build

--- tests/build.cpp
+++ tests/build.cpp
@@ -1,10 +1,24 @@
 #include "main.h"
 
-// Build BPlusTree 10 times using order=128
+// Build BPlusTree using config specified by args or default
 // Report average time and average performance
-int main() {
+int main(int argc, char *argv[]) {
+	int order = 128, runs = 10;
+	if (argc > 1) {
+		// Parse args
+		for (int i=1; i<argc; i++) {
+			std::string arg = argv[i];
+			if (arg == "-o") {
+				order = std::stoi(argv[i+1]);
+			}
+			else if (arg == "-r") {
+				runs = std::stoi(argv[i+1]);
+			}
+		}
+	}
 	std::cout << "----------test-build----------\n";
-	std::cout << "BPlusTree: order=128\n";
+	std::cout << "BPlusTree: order=" << order <<"\n";
+	std::cout << "Doing " << runs << "runs(s)\n";
 	std::chrono::duration<double, std::milli> tot_time;
 	for (int i=0; i < 10; i++) {
 		BPlusTree tree(128);
@@ -14,6 +28,7 @@ int main() {
 	}
 	std::chrono::duration<double, std::milli> avg_time = tot_time/10;
 	double m_ops = 1000/avg_time.count();
+	double k_ops = m_ops*1000;
 	std::cout << "Average build time: " << avg_time.count() << "ms\n";
-	std::cout << "Avarage insert performance: " << m_ops << " MOps/sec" << std::endl;
+	std::cout << "Average insert performance: " << k_ops << " KOps/sec" << std::endl;
 }
