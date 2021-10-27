5dcafec2bbc8ce966e3b7354bd2928725d6c229a
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2021-10-27 13:19:26 +0200

    add additional options to parallel search test

--- tests/parallel-search.cpp
+++ tests/parallel-search.cpp
@@ -3,12 +3,17 @@
 
 int main(int argc, char *argv[]) {
 	int trees = 4, order = 128, searches = 100000, scans = 10;
+	std::string file = "input_files/input_file_1000000.txt", search_method = "rand";
 	if (argc > 1) {
 		// Parse args
 		for (int i=1; i<argc; i++) {
 			std::string arg = argv[i];
 			if (arg == "-t") {
 				trees = std::stoi(argv[i+1]);
+			}
+			else if (arg == "-f") {
+				std::string file_name = argv[i+1];
+				file = "input_files/" + file_name;
 			}
 			else if (arg == "-o") {
 				order = std::stoi(argv[i+1]);
@@ -19,6 +24,9 @@ int main(int argc, char *argv[]) {
 			else if (arg == "-sc") {
 				scans = std::stoi(argv[i+1]);
 			}
+			else if (arg == "-sm") {
+				search_method = argv[i+1];
+			}
 		}
 	}
 	std::cout << "----------test-parallel-search----------\n";
@@ -28,19 +36,35 @@ int main(int argc, char *argv[]) {
 	std::cout << "Building tree...\n";
 
 	ParallelBPlusTree ptree(order, trees, 1000000);
-	auto build_time = ptree.build("input_files/input_file_1000000.txt");
+	auto build_time = ptree.build(file);
 	std::cout << "Build took: " << build_time.count() << "ms" << "\n";
 
 	std::chrono::duration<double, std::micro> tot_search_time;
 	std::cout << "Searching tree...\n";
-	for (int i=0; i < searches; i++) {
-		float key = -1500 + static_cast <float> (rand())/(static_cast <float> (RAND_MAX/3000));
-		key = roundf(key * 100) / 100;
-		auto t1 = std::chrono::high_resolution_clock::now();
-		ptree.search(key);
-		auto t2 = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double, std::micro> search_time = t2 - t1;
-		tot_search_time += search_time;
+	if (search_method == "rand") {
+		for (int i=0; i < searches; i++) {
+			float key = -1500 + static_cast <float> (rand())/(static_cast <float> (RAND_MAX/3000));
+			key = roundf(key * 100) / 100;
+			auto t1 = std::chrono::high_resolution_clock::now();
+			ptree.search(key);
+			auto t2 = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double, std::micro> search_time = t2 - t1;
+			tot_search_time += search_time;
+		}
+	}
+	else if (search_method == "prim") {
+		std::random_device rd;
+		std::mt19937 rng(rd());
+		std::uniform_int_distribution<int> uni(0, 999999);
+		for (int i=0; i < searches; i++) {
+			auto random_integer = uni(rng);
+			float key = static_cast<float>(random_integer);
+			auto t1 = std::chrono::high_resolution_clock::now();
+			ptree.search(key);
+			auto t2 = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double, std::micro> search_time = t2 - t1;
+			tot_search_time += search_time;
+		}
 	}
 	double ops = searches/(tot_search_time.count()/1000000);
 	double k_ops = ops/1000;
